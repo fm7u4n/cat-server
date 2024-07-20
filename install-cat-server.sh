2855f95105221b61be1042e022947bb8d2087a29
gitea-admin <wangyang@safeis.cn>
2024-07-20 15:56:03 +0800

    init

--- install-cat-server.sh
+++ install-cat-server.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-set -x
+set -eu
 
 install_depend() {
+	echo "install depend"
 	apt update && \
 	apt install -y openjdk-11-jre-headless && \
 	apt install -y openjdk-11-jdk
@@ -30,22 +31,20 @@ EOF
 gen_catserver_config() {
 	IP="$(curl ip.sb)"
 	cat > ${WorkingDirectory}/CatServer.properties << EOF
-CatServer.Version = 1.1
+CatServer.host = $IP
 CatServer.port = 5000
+CatServer.profile-name = admin
+CatServer.password = mhxzkhl9521
+CatServer.Version = 1.1
 CatServer.store = feifeibufei.store
 CatServer.store-password = zjUFG6ITfHtgsT
-CatServer.host = $IP
-CatServer.password = mhxzkhl9521
-CatServer.profile-name = admin
 CatServer.profile = test.profile
 CatServer.auth = false
 CatServer.authlog = false
 CatServer.googleauth = false
 CatServer.googlekey = YOTPPRZ4RQ75QNKKE65GXE6BQBSQDVQJ
 CatServer.safecode = MkbY0wttL86Z8
-
 CatServer.Iv = abcdefghijklmnop
-
 stager.checksum-num = 400
 stager.x86-num = 100
 stager.x86-uri-len = 6
@@ -66,11 +65,15 @@ main() {
 	else
 		mkdir ${WorkingDirectory}
     fi
-    curl https://raw.githubusercontent.com/fm7u4n/cat-server/main/CS4.5/cat_server.tar -o /tmp/cat-server.tar
+	echo "download cat_server"
+    curl https://media.githubusercontent.com/media/fm7u4n/cat-server/main/CS4.5/cat_server.tar?download=true -o /tmp/cat-server.tar
     tar -xvf /tmp/cat-server.tar -C ${WorkingDirectory} --strip-components=1
 	rm /tmp/cat-server.tar
-
 	chmod +x ${WorkingDirectory}/teamserver
+	if [ ! $? -eq 0 ]; then
+		echo "Installation failure"
+		exit 1
+	fi
 
 	gen_catserver_config && \
 	gen_systemd_service && \
@@ -79,6 +82,11 @@ main() {
 	systemctl start cat-server.service
 
 	echo "Installation complete"
+	grep -E "^CatServer\.(host|port|profile-name|password)" ${WorkingDirectory}/CatServer.properties | \
+    sed 's|CatServer.host =|Host:|;
+    	 s|CatServer.port =|Port:|;
+    	 s|CatServer.profile-name =|User:|;
+    	 s|CatServer.password =|Password:|;'
 }
 
 main $@
